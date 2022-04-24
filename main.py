38114e2160ab993899b4996dcf704cf402f6925d
merrickhare <merrickhare@outlook.com>
2022-04-23 18:14:44 -0600

    next update for UI

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import pyautogui as PAG
 import PySimpleGUI as pg
+import pyautogui as pag
 
 
-layout = [[pg.Text("Mouse Wiggle App")],[pg.Checkbox("Start",default=False,key="CheckOne")],[pg.Text("Interval")],[pg.Button("Close")]]
 
-pg.Window("Mouse Wiggle",layout=layout,resizable=True).read()
+layout = [[pg.Text("Choose Interval")],[pg.Radio("1 Second","Group1")],[pg.Radio("3 Second","Group1")],[pg.Radio("5 Second","Group1")],[[pg.Button("Start",key="start_button")],[pg.Button("Stop",key="stop_button")]]]
+
+pg.Window("Mouse Wiggle",layout=layout,resizable=True,size=(300,175)).read()
 
 
 
