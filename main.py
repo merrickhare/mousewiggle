5c4755a5f47f333c9666cb14d8b47fc4b1c6d6b3
merrickhare <merrickhare@outlook.com>
2022-04-23 20:15:22 -0600

    created temporary function to print interval

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from optparse import Values
 import PySimpleGUI as pg
 import pyautogui as pag
 
 
+def wiggle(intervalNumber):
+    print(intervalNumber)
 
+pg.theme('Tan')
 layout = [[pg.Text("Choose Interval"),pg.Radio("1 Second","Group1",key="radio1"),pg.Radio("3 Second","Group1",key="radio3"),pg.Radio("5 Second","Group1",key="radio5")],[pg.Button("Start",key="start_button"),pg.Button("Stop",key="stop_button")]]
 
 window = pg.Window("Mouse Wiggler", layout=layout, size=(375,75))
@@ -10,10 +14,23 @@ window = pg.Window("Mouse Wiggler", layout=layout, size=(375,75))
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
+
     if event == pg.WIN_CLOSED or event == 'stop_button':
         break
-    if event == 'start_button':
-        print("hello world")
+    elif event == 'start_button' and values["radio1"] == True:
+        wiggle("1 second")
+    elif event == 'start_button' and values["radio3"] == True:
+        wiggle("3 second")
+    elif event == 'start_button' and values["radio5"] == True:
+        wiggle("5 second interval")
+
+    
+
+    
+
+   
+    
+
 
 
   
