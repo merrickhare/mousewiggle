341c522559e142dfe30c0485f7cfecaca510644c
merrickhare <merrickhare@outlook.com>
2022-04-23 18:27:58 -0600

    event loop and close window functionality

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import pyautogui as pag
 
 
 
-layout = [[pg.Text("Choose Interval")],[pg.Radio("1 Second","Group1")],[pg.Radio("3 Second","Group1")],[pg.Radio("5 Second","Group1")],[[pg.Button("Start",key="start_button")],[pg.Button("Stop",key="stop_button")]]]
+layout = [[pg.Text("Choose Interval"),pg.Radio("1 Second","Group1",key="radio1"),pg.Radio("3 Second","Group1",key="radio3"),pg.Radio("5 Second","Group1",key="radio5")],[pg.Button("Start",key="start_button"),pg.Button("Stop",key="stop_button")]]
 
-pg.Window("Mouse Wiggle",layout=layout,resizable=True,size=(300,175)).read()
+window = pg.Window("Mouse Wiggler", layout=layout, size=(375,75))
+
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == pg.WIN_CLOSED or event == 'stop_button':
+        break
+   
+
 
 
 
